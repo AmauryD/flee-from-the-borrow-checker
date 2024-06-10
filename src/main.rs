d3261b31525cd7cf970ad95f4b5199c6a0e06965
AmauryD <deflorenne.amaury@gmail.com>
2024-06-10 11:51:54 +0200

    init

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+const MATH_PI: f64 = 3.14159265359;
+
+/*
+    Les constantes
+*/
+fn main() {
+    // ignore le fait que cette variable soit non utilisée.
+    let _a = 1;
+
+    let ma_variable = 1;
+
+    println!("{ma_variable}");
+}
